6f939e75809171f3e3492b1211abbd774c07fe9e
taciturn02 <taciturn248@gmail.com>
2023-12-21 18:25:27 +0530

    user model is created successfully in server

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+//initializing mongoose
+const mongoose = require("mongoose");
+
+//creating user schema
+const userSchema = new mongoose.Schema({
+
+        firstName: {
+            type:String,
+            required:true,
+            trim:true,
+        },
+        lastName:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        accountType:{
+            type:String,
+            enum:["Admin","Student","Instructor"],
+            required:true
+        },
+        additionalDetails:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"Profile",
+        },
+        courses:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Course",
+            }
+        ],
+        image:{
+            type:String,
+            required:true,
+        },
+        courseProgress:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Course Progress",
+            }
+        ]
+});
+//exporting the created schema
+module.exports = mongoose.model("User",userSchema);
