ac74b440c2dd3e2aac959b0b43afa2e4c45c16a8
taciturn02 <taciturn248@gmail.com>
2023-12-22 12:31:55 +0530

    Controllers of profile,section and subsection created successfully

---
+++ server/controllers/Profile.js
@@ -0,0 +1,111 @@
+const Profile = require("../models/Profile");
+const User = require("../models/User");
+
+exports.updateProfile = async(req,res)=>{
+    try{
+            //get data
+            const {dateOfBirth="",about = "",contactNumber,gender} = req.body;
+
+            //get userId
+            const id = req.user.id;
+
+            //validation
+            if(!contactNumber || !gender){
+                return res.status(400).json({
+                    success:false,
+                    message : "All fields are required",
+                })
+            }
+
+            //find profile
+            const userDetails = await User.findById(id);
+            const profileId  = userDetails.additionalDetails;
+            const profileDetails  = await Profile.findById(profileId);
+
+            //update profile
+            profileDetails.dateOfBirth = dateOfBirth;
+            profileDetails.about = about;
+            profileDetails.gender = gender;
+            profileDetails.contactNumber = contactNumber;
+            await profileDetails.save();
+
+            //return response
+            return res.status(200).json({
+                success : true,
+                message : "profile updated successfully",
+                profileDetails,
+            });
+    }
+    catch(error){
+        return res.status(500).json({
+            success : false,
+            message :"profile is NOT updated",
+            error:error.message
+        });
+    }
+};
+
+//deleteAccount
+exports.deleteAccount = async(req,res) =>{
+    try{
+            //get id
+            const id = req.user.id;
+            //validation
+            const userDetails = await User.findById(id);
+            if(!userDetails){
+                return res.status(404).json({
+                    success:false,
+                    message : "User Not Found",
+                });
+            }
+            //delete profile
+            await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+
+            //TODO  :: HW UNenroll user from all the enrolled courses
+
+            //delete user 
+            await User.findByIdAndDelete({_id:id});
+          
+            //return res
+            return res.status(200).json({
+                success : true,
+                message : "User Deleted Successfully",
+            })
+    }
+    catch(error){
+            return res.status(400).json({
+                success:false,
+                message:"User cannot be deleted"
+            });
+    }
+};
+
+
+//getALLDetailsOfUser
+
+exports.getUserAllDetails = async(req,res) =>{
+
+        try{
+                //get id
+                const id = req.user.id;
+
+                //validation
+                const userDetails = await User.findById(id).populate("additionalDetails").exec();
+
+                //return response
+                return res.status(200).json({
+                    success : true,
+                    message : "User Data fetched Successfully",
+                });
+                
+
+        }
+        catch(error){   
+
+            return res.status(400).json({
+                success : false,
+                message : "User Details cannot fetched "
+            })
+
+        }   
+}
---
+++ server/controllers/Section.js
@@ -0,0 +1,103 @@
+const Section = require("../models/Section");
+const Course = require("../models/Course");
+
+
+//CREATE HANDLER FUNCTION
+
+exports.createSection = async (req,res)=>{
+    try{
+
+            //data fetch
+            const {sectionName,courseId} = req.body;
+            //data validation
+            if(!sectionName || !courseId){
+                return res.status(400).json({
+                    success:false,
+                    message : "All fields are required",
+                });
+            }
+            //create section
+            const newSection = await Section.create({sectionName});
+            //update course with section ObjectId
+            const updatedCourseDetails = await Course.findByIdAndUpdate(
+                                                    courseId,
+                                                    {
+                                                        $push:{
+                                                            courseContent:newSection._id,
+                                                        }
+                                                    },
+                                                    {new:true},
+                                                    );
+           // HW : use populate to replace section/subsection both in updatedCourseDetails
+           // return response
+           return res.status(200).json({
+                success : true,
+                message : 'Section created successfully',
+                updatedCourseDetails,
+           })
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message : 'unable to create section,try again later',
+            error:error.message,
+        });
+    }
+}
+
+//UPDATE HANDLER FUNCTION
+
+exports.updateSection = async (req,res) =>{
+    try{
+            //data input
+            const {sectionName,sectionId} = req.body;
+            //data validation
+            if(!sectionName || !sectionId){
+                return res.status(400).json({
+                    success:false,
+                    message : "All fields are required",
+                });
+            }
+            //update data
+            const section = await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+               
+            //return response
+
+            return res.status(200).json({
+                success : true,
+                message : 'Section Updated Successfully',
+            });
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message : 'unable to update section,try again later',
+            error:error.message,
+        });
+    }
+}
+
+//DELETE HANDLER FUNCTION
+exports.deleteSection = async (req,res) => {
+    try{
+
+            //get ID - assuming that we are sending ID in params
+            const {sectionId} = req.params
+            //use findByIdAndDelete
+            await Section.findByIdAndDelete(sectionId);
+            //return response
+
+            //TODO - DO WE NEED TO DELETE THE ObjectID OF Section FROM COURSE SCHEMA
+            return res.status(200).json({
+                success : true,
+                message : 'Section Deleted Successfully',
+            })
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message : 'unable to delete section,try again later',
+            error:error.message,
+        });
+    }
+}
