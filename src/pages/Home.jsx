cd473e64dcd5c530851002fe4d5996e6e4153ec1
Deepak Chauhan <deepakchauhan4572@gmail.com>
2024-01-06 00:24:17 +0530

    Login and SignUp Page created successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 // Icons Import
+import React from 'react'
 import { FaArrowRight } from "react-icons/fa"
 import { Link } from "react-router-dom"
+import HighlightText from '../components/core/HomePage/HighlightText'
 
 // Image and Video Import
 import Banner from "../assets/Images/banner.mp4"
@@ -9,7 +11,6 @@ import Footer from "../components/common/Footer"
 import CTAButton from "../components/core/HomePage/Button"
 import CodeBlocks from "../components/core/HomePage/CodeBlocks"
 import ExploreMore from "../components/core/HomePage/ExploreMore"
-import HighlightText from "../components/core/HomePage/HighlightText"
 import InstructorSection from "../components/core/HomePage/InstructorSection"
 import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection"
 import TimelineSection from "../components/core/HomePage/TimelineSection"
