2a3d6174a1b6a0171e8357b75859bf4260488caa
taciturn02 <taciturn248@gmail.com>
2024-01-23 18:52:40 +0530

    Course,Payment,Profile,User Routes Created successfully

---
+++ server/routes/User.js
@@ -0,0 +1,21 @@
+
+// Route for user signup
+router.post("/signup", signup)
+
+// Route for sending OTP to the user's email
+router.post("/sendotp", sendotp)
+
+// Route for Changing the password
+router.post("/changepassword", auth, changePassword)
+
+
+//RESET PASSWORD
+
+// Route for generating a reset password token
+router.post("/reset-password-token", resetPasswordToken)
+
+// Route for resetting user's password after verification
+router.post("/reset-password", resetPassword)
+
+// Export the router for use in the main application
+module.exports = router
